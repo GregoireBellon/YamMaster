15e2fcb080b8c3b72f764caa541c4bf5823c6649
Grégoire Bellon <gregoire.bellon@outlook.fr>
2019-11-06 13:52:03 +0100

    Ajout page

---
+++ app/src/main/java/com/example/yatzy/MenuJouer.java
@@ -0,0 +1,14 @@
+package com.example.yatzy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuJouer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_jouer);
+    }
+}
