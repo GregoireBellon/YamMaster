b1b1a78ae8f4f9f26c6ef143f42c83016bcac85e
AresEntretainement <35566353+AresEntretainement@users.noreply.github.com>
2019-11-06 13:48:07 +0100

    Debut ded l'implementation des interface Ecran et Entite

---
+++ app/src/main/java/com/example/yatzy/EcranEdition.java
@@ -0,0 +1,21 @@
+package com.example.yatzy;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EcranEdition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ecran_accueil);
+        getSupportActionBar().hide();
+    }
+
+}
+
---
+++ app/src/main/java/com/example/yatzy/Entite.java
@@ -0,0 +1,55 @@
+package com.example.yatzy;
+
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Entite {
+
+    /*
+    Ecran context;
+    ImageView sprite;
+
+    public Entite(AppCompatActivity context)
+    {
+        this.context = context;
+        this.sprite = new ImageView(context);
+    }
+
+    public void setSprite(int sprite)
+    {
+        this.sprite = new ImageView(context);
+        this.sprite.setImageResource(sprite);
+    }
+
+    public void setSize(int width, int height)
+    {
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(width, height);
+        this.sprite.setLayoutParams(params);
+    }
+
+    public void display()
+    {
+        context.fenetrePrincipale.addView(this.sprite);
+    }
+
+    public float getX()
+    {
+        return this.sprite.getX();
+    }
+    public float getY()
+    {
+        return this.sprite.getY();
+    }
+    public void setX(float x)
+    {
+        this.sprite.setX(x);
+    }
+    public void setY(float y)
+    {
+        this.sprite.setY(y);
+    }
+
+*/
+}
