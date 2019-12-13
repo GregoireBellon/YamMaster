f3a28c0f0fe26c5ff4a450568f5b7856f2ad20ea
NothonIUT <lemoannathan@gmail.com>
2019-12-13 10:39:32 +0100

    Interface Plateau terminée

--- app/src/main/java/com/example/yatzy/Case.java
+++ app/src/main/java/com/example/yatzy/Case.java
@@ -7,8 +7,75 @@ public class Case {
 
     private int coordX, coordY;
     private TypeCase type;
-    private boolean jetonPose;
-    private Jeton
+    private boolean caseRemplie;
+    private boolean peutPoser;
+    private Jeton jetonPose;
+
+    public Case(TypeCase type) {
+        this.type = type;
+        this.caseRemplie = false;
+        this.peutPoser = false;
+        this.jetonPose = null;
+    }
+
+    public void poserJeton(Jeton jeton){
+        setJetonPose(jeton);
+        setCaseRemplie(true);
+        setPeutPoser(false);
+    }
+
+    public void retirerJeton(){
+        setJetonPose(null);
+        setCaseRemplie(true);
+        setPeutPoser(true);
+    }
 
 
+
+
+    // ---------------------------------------- Getters et Setters
+
+    public int getCoordX() {
+        return coordX;
+    }
+
+    public int getCoordY() {
+        return coordY;
+    }
+
+    public TypeCase getType() {
+        return type;
+    }
+
+    public boolean isCaseRemplie() {
+        return caseRemplie;
+    }
+
+    public boolean isPeutPoser() {
+        return peutPoser;
+    }
+
+    public Jeton getJetonPose() {
+        return jetonPose;
+    }
+
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
+
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
+
+    public void setCaseRemplie(boolean caseRemplie) {
+        this.caseRemplie = caseRemplie;
+    }
+
+    public void setPeutPoser(boolean peutPoser) {
+        this.peutPoser = peutPoser;
+    }
+
+    public void setJetonPose(Jeton jetonPose) {
+        this.jetonPose = jetonPose;
+    }
 }
