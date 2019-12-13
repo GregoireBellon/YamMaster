adcd79fb2eec71ff15bc9e30fc0d932a0898487e
Grégoire Bellon <gregoire.bellon@outlook.fr>
2019-12-13 09:36:36 +0100

    Debut d'interface?

---
+++ app/src/main/java/com/example/yatzy/model/Scanner_perso.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Scanner_perso {
+
+	public static String Reponse()
+	{
+	Scanner sc = new Scanner(System.in);
+	String rep;
+	rep = sc.nextLine();
+	return rep;
+	}
+}
